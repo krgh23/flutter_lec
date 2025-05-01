6d51e0739f05dfcdd11d30d162bc544025f0eda4
krgh23 <krgh23@naver.com>
2025-05-01 13:35:58 +0900

    20250501

--- k_file_io/android/app/build.gradle.kts
+++ k_file_io/android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.min.j_hive_provider"
+    namespace = "com.min.k_file_io"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.min.j_hive_provider"
+        applicationId = "com.min.k_file_io"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
